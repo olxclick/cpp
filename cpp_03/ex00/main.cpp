e7006775dc533415f9858f9116ce6ab9969d1b5f
olxclick <joaolazaro720@gmail.com>
2023-12-13 14:47:26 +0000

    ex00 from cpp03 done

--- cpp_03/ex00/main.cpp
+++ cpp_03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jbranco- <jbranco-@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 13:42:07 by jbranco-          #+#    #+#             */
-/*   Updated: 2023/12/05 13:53:08 by jbranco-         ###   ########.fr       */
+/*   Updated: 2023/12/13 14:47:17 by jbranco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 int main()
 {
-	
+	ClapTrap	var("Pedro");
+
+	var.attack("Someone");
+	var.takeDamage(11);
+	var.attack("Someone");
+	var.beRepaired(5);
 }
